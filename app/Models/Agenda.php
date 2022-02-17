2a1a855d2ff7e7645fdf60ae8f4cb3e5852a59cf
15fpereira <franciscod.pereira@outlook.com>
2022-02-16 23:12:51 -0300

    Criar os metodos de relacionamento um para muitos tabelas chamados e agendas.

--- app/Models/Agenda.php
+++ app/Models/Agenda.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Agenda extends Model
 {
     use HasFactory;
+    /**
+     * Get the colaborador that owns the Colaborador
+     * Importante que o nome da chave estrageira seja o mesmo nome do metodo seguido de _id.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function chamados()
+    {
+        return $this->belongsTo(Chamado::class);
+    }
 }
--- app/Models/Chamado.php
+++ app/Models/Chamado.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Chamado extends Model
 {
     use HasFactory;
+          /**
+     *
+     * Neste caso, o metodos precisa estar no plural. Mostra todos os colaboradores do departamento.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+
+    public function agendas()
+    {
+        return $this->hasMany(Agenda::class);
+        //return $this->belongsTo(Agenda::class);
+
+    }
 }
