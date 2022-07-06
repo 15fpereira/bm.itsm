9a2022d5231fc7c3a0717b0793d287d2318f27b3
Francisco Dias de Asis Pereira <francisco.pereira@bit.local>
2022-07-06 13:33:16 -0300

    Alteração no projeto

--- resources/views/agenda/edit.blade.php
+++ resources/views/agenda/edit.blade.php
@@ -25,14 +25,18 @@
             <div class="row">
                 <div class="col-4">
                     <div class="form-group">
+                    <fieldset @if($chamado->status == "Concluido" or Auth::user()->status == 'padrao') disabled="" @endif>
                         <label for="exampleTextarea" class="form-label mt-4"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Observação:</font></font></label>
                         <textarea class="form-control" id="observacao" name="observacao" value="{{$agenda->observacao}}" placeholder="{{$agenda->observacao}}" rows="3">{{$agenda->observacao}}</textarea>
+                    </fieldset>
                     </div>
                 </div>
                 <div class="col-8">
                     <div class="form-group">
+                    <fieldset @if($chamado->status == "Concluido" or Auth::user()->status == 'padrao') disabled="" @endif>
                         <label for="exampleTextarea" class="form-label mt-4"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Descrição:</font></font></label>
                         <textarea class="form-control" id="descricao" name="descricao" value="{{$agenda->descricao}}" placeholder="{{$agenda->descricao}}" rows="3">{{$agenda->descricao}}</textarea>
+                    <fieldset>
                     </div>
                 </div>
             </div>
@@ -40,6 +44,7 @@
 
 
                 <!-- Home Radios -->
+                <fieldset @if($chamado->status == "Concluido" or Auth::user()->status == 'padrao') disabled="" @endif>
                 <legend class="mt-4"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Situação da agenda atual:</font></font></legend>
                 <div class="form-check form-check-inline">
                     <label class="form-check-label">
@@ -59,10 +64,13 @@
                     Concluido:
                     </font></font></label>
                 </div>
+                <fieldset>
             <!-- End Radios -->
         </div>
         <div class="modal-footer">
-          <button type="submit" class="btn btn-primary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Salvar alterações</font></font></button>
+
+          <button type="submit" @if($chamado->status == "Concluido" or Auth::user()->status == 'padrao') class="btn btn-primary disabled" @else class="btn btn-primary" @endif><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Salvar alterações</font></font></button>
+
           <button type="button" class="btn btn-secondary" data-dismiss="modal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Sair</font></font></button>
         </div>
             </form>
